4339f7eeff8f3204f9b40c32c59b1c6b12528bbf
Steve Hook <steve.hook@gmail.com>
2016-01-22 12:27:43 +0000

    Add a seed npm task

--- server/lib/database.js
+++ server/lib/database.js
@@ -1,3 +1,3 @@
 const massive = require('massive');
-const connectionString = `postgres://stevehook:@localhost/node_todo_${process.env.NODE_ENV}`;
+const connectionString = `postgres://stevehook:@localhost/node_todo_${process.env.NODE_ENV || 'development'}`;
 module.exports = massive.connectSync({connectionString : connectionString});

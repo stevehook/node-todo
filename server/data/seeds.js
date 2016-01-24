1afec56c71c07acceb89d640f552bd297a6acb61
Steve Hook <steve.hook@gmail.com>
2016-01-24 19:24:54 +0000

    Add some sample tasks to the seed data

--- server/data/seeds.js
+++ server/data/seeds.js
@@ -8,7 +8,17 @@ const db = require('../lib/database');
 db.users.destroy({}, (err, _) => {
   hashPassword('secret', (err, hash) => {
     db.users.save({ name: 'Bob', email: 'bob@example.com', password: hash }, (err, user) => {
-      process.exit();
+      let taskFixtures = [
+        { user_id: user.id, title: 'Walk the dog', completed: false, order: 1 },
+        { user_id: user.id, title: 'Make the dinner', completed: false, order: 1 },
+        { user_id: user.id, title: 'Do the washing up', completed: false, order: 1 }
+      ];
+      taskFixtures.reverse().reduce((last, taskFixture) => {
+        return () => { db.tasks.save(taskFixture, (err, task) => {
+            last();
+          });
+        };
+      }, () => process.exit())();
     });
   });
 });

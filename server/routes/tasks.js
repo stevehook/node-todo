3ac3d2cecfb9cfdcf755330d12ea47bf79b6a627
Steve Hook <steve.hook@gmail.com>
2016-01-25 12:40:24 +0000

    WIP refactor to toggle between HTML/JSON responses

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -4,10 +4,30 @@ const express = require('express');
 const router = express.Router();
 const authenticate = require('../lib/authenticate');
 
+const taskRepository = {
+  get: (request, callback) => {
+    let db = request.app.get('db');
+    db.tasks.findOne({ id: request.params['id'], user_id: request.currentUser.id }, (err, task) => {
+      callback(err, 200, task);
+    });
+  },
+};
+
+const respondWith = (request, response, code, data) => {
+  if (request.accepts('json')) {
+    response.status(code).json(data);
+  } else {
+    response.render(code, data);
+  }
+};
+
 router.get('/task/:id', authenticate, (request, response) => {
-  let db = request.app.get('db');
-  db.tasks.findOne({ id: request.params['id'], user_id: request.currentUser.id }, (err, task) => {
-    response.status(200).json(task);
+  taskRepository.get(request, (err, code, data) => {
+    if (err) {
+      respondWith(request, response, 500, {});
+    } else {
+      respondWith(request, response, code, data);
+    }
   });
 });
 
